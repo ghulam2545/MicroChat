f0aa174da788aa53c26d8a3ea81327db6fd626b5
ghulam2545 <ghulam2545@gmail.com>
2023-12-14 19:48:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Mitter
+
+a RESTful API for a Twitter-like social media platform using Java Spring Boot and Maven.
+Mitter is taken from 'my twitter'.
---
+++ src/main/java/com/ghulam/mitter/MitterApplication.java
@@ -0,0 +1,13 @@
+package com.ghulam.mitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitterApplication.class, args);
+	}
+
+}
